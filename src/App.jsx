551cea185c1f75c78e762a1697d6edda5ba4e16f
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-20 17:44:00 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-white bg-gray- text-4xl p-4 font-bold">Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
