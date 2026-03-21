968a542105c918a24072a71ae60f88468f1deffa
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-21 15:46:46 +0530

    Add Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react"
+import { Outlet } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth/auth"
+import { login, logout } from "./store/authSlice"
+import {Header, Footer} from "./components/index"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) dispatch(login({userData}))
+      else dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  }, [dispatch])
 
-  return (
-    <>
-      <h1 className="text-center text-white bg-gray- text-4xl p-4 font-bold">Blog App with Appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="bg-gray-700 text-white min-h-screen flex justify-center">
+      <div>
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
